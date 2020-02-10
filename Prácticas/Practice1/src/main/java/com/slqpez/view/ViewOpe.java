1e5d99eba2e2e90f3595bef25eebfe6650190be6
slqpez <santiago.lopez16@udea.edu.co>
2020-02-09 18:07:52 -0600

    Comentando método another

--- Prácticas/Practice1/src/main/java/com/slqpez/view/ViewOpe.java
+++ Prácticas/Practice1/src/main/java/com/slqpez/view/ViewOpe.java
@@ -27,27 +27,27 @@ public class ViewOpe {
         p.mostrarResta(rr);
     }
 
-    public void anotherProcess()  {
-        System.out.println("Ingrese ´1´ si desea sumar, o ´2´ si desea restar: ");
-        char choise = n.nextLine().charAt(0);
-        if (choise == '1') {
-            System.out.println("Ingrese la pareja de números que desee sumar.");
-            System.out.print("Ingrese el primer número: ");
-            s.setA(Integer.parseInt(n.nextLine()));
-            System.out.print("");
-            System.out.print("Ingrese el segundo número: ");
-            s.setB(Integer.parseInt(n.nextLine()));
-            int rs = p.sumar(s);
-            p.mostrarSuma(rs);
-        } else if (choise == '2') {
-            System.out.println("Ingrese la pareja de números que desee restar.");
-            System.out.print("Ingrese el primer número: ");
-            r.setMinuendo(Integer.parseInt(n.nextLine()));
-            System.out.print("");
-            System.out.print("Ingrese el segundo número: ");
-            r.setSustraendo(Integer.parseInt(n.nextLine()));
-            int rr = p.restar(r);
-            p.mostrarResta(rr);
-        }
-    }
+//    public void anotherProcess()  {
+//        System.out.println("Ingrese ´1´ si desea sumar, o ´2´ si desea restar: ");
+//        char choise = n.nextLine().charAt(0);
+//        if (choise == '1') {
+//            System.out.println("Ingrese la pareja de números que desee sumar.");
+//            System.out.print("Ingrese el primer número: ");
+//            s.setA(Integer.parseInt(n.nextLine()));
+//            System.out.print("");
+//            System.out.print("Ingrese el segundo número: ");
+//            s.setB(Integer.parseInt(n.nextLine()));
+//            int rs = p.sumar(s);
+//            p.mostrarSuma(rs);
+//        } else if (choise == '2') {
+//            System.out.println("Ingrese la pareja de números que desee restar.");
+//            System.out.print("Ingrese el primer número: ");
+//            r.setMinuendo(Integer.parseInt(n.nextLine()));
+//            System.out.print("");
+//            System.out.print("Ingrese el segundo número: ");
+//            r.setSustraendo(Integer.parseInt(n.nextLine()));
+//            int rr = p.restar(r);
+//            p.mostrarResta(rr);
+//        }
+//    }
 }
