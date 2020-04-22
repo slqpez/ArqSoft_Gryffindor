4609a5f612721e010b599837da8aacb8a487983a
Alejandro <mello0616@gmail.com>
2020-04-22 17:26:14 -0500

    following hagrid version actual

---
+++ Following Hagrid/public/js/mainAdmin.js
@@ -0,0 +1,70 @@
+var map;
+var socket = io.connect('http://localhost:3000', {'forceNew':true});
+function initMap(){
+    const ubicacion = new Localizacion( ()=>{
+        const myLatLng = {
+            lat: ubicacion.latitude,
+            lng: ubicacion.longitude
+        };
+
+    var options = {
+        center:myLatLng ,
+        zoom:15
+    }
+
+    map = new google.maps.Map(document.getElementById('map'), options);
+    
+    
+    const marcador = new google.maps.Marker({
+        position:myLatLng,
+        map:map,
+        title:"Tu posicion como Empresario macho que sos",
+    });
+
+    });
+    
+}
+
+socket.on('newPosition',(position)=>{
+  console.log("conductor conectado");
+  var imagen = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';  
+  var texto = position[1];
+  var pos = position[0];
+  
+  var marcador = new google.maps.Marker({
+    position: pos,
+    map: map,
+    title:"conductor",
+    icon:imagen
+   });
+
+   var options = {//opciones de la nueva pocision
+    map: map,
+    position: pos,
+    center: pos
+   };
+   var informacion= new google.maps.InfoWindow({
+    content:texto
+  });
+
+  marcador.addListener('click',function(){
+    informacion.open(map,marcador);
+  });  
+});
+
+
+function handleNoGeolocation(errorFlag) 
+{
+	  if (errorFlag) 
+	  {
+		var content = 'Error: The Geolocation service failed.';
+	  } 
+	  else 
+	  {
+		var content = 'Error: Your browser doesn\'t support geolocation.';
+	  }
+	
+	  var infowindow = new google.maps.InfoWindow(options);
+	  map.setCenter(options.position);
+}
+
---
+++ Following Hagrid/public/js/mainConductor.js
@@ -0,0 +1,81 @@
+var map;
+var marker;
+var imagen = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+var socket = io.connect('http://localhost:3000', {'forceNew':true});
+
+function initMap(){
+    const ubicacion = new Localizacion( ()=>{
+        const myLatLng = {
+            lat: ubicacion.latitude,
+            lng: ubicacion.longitude
+        };
+
+    var options = {
+        center:myLatLng ,
+        zoom:15
+    }
+
+    map = new google.maps.Map(document.getElementById('map'), options);
+    
+    const marcador = new google.maps.Marker({
+        position:myLatLng,
+        map:map,
+        title:"tu posicion",
+        icon:imagen
+
+        
+    });
+
+    });
+	 
+}
+
+function animar(){//funcion crea un nuevo marcador en el mapa
+
+    const ubicacion = new Localizacion( ()=>{
+        const pos = {
+            lat: ubicacion.latitude,
+            lng: ubicacion.longitude
+        };
+        var texto = '<h1> Nombre conductor </h1>' + '<h5>Placa camion</h5>'+'<h5>Empresa del camion</h5>'+'<h5>no recuerdo que mas poner</h5>';
+        const position =[pos,texto];
+        console.log(position);   
+        var marker = new google.maps.Marker({
+			position: pos,
+            map: map,
+            title:"Tu posicion",
+            animation: google.maps.Animation.DROP,
+            icon:imagen
+           
+		  });		
+		
+	    var options = {//opciones de la nueva pocision
+			map: map,
+			position: pos,
+			center:pos
+          };
+        var informacion = new google.maps.InfoWindow({
+            content:texto
+        });
+        marker.addListener('click',function(){
+            informacion.open(map,marker);
+        });
+         //enviamos al socket la nueva pocision	  
+        socket.emit('position',position);
+    });
+}
+
+function handleNoGeolocation(errorFlag) 
+{
+	  if (errorFlag) 
+	  {
+		var content = 'Error: The Geolocation service failed.';
+	  } 
+	  else 
+	  {
+		var content = 'Error: Your browser doesn\'t support geolocation.';
+	  }
+
+}
+
+setInterval(function(){animar()}, 3000);//cada 3 segundos extraemos la ubicacion nuevamente
